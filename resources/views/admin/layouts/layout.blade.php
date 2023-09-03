e39868314d9b5e4328046f9ccc615e76dba1b510
ostenbaken87 <ostenbaken87@mail.ru>
2023-09-03 23:47:00 +0800

    first steps

---
+++ resources/views/admin/layouts/layout.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AdminPanel</title>
+    <link rel="stylesheet" href="{{asset('assets/admin/src/fontawesome-free/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/admin/css/style.css')}}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;700&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="wrapper">
+    <header class="header">
+        <div class="header_navbar wrapper_content">
+            <div class="header_navbar_logo">
+                <div class="header_navbar_logo_icon">
+                    <i class="fas fa-ghost"></i>
+                </div>
+                <div class="header_navbar_logo_brand">
+                    <a href="#">AdminPanel</a>
+                </div>
+            </div>
+            <div class="header_navbar_links">
+                <div class="header_navbar_links_link">
+                    <a href="index.html">Home</a>
+                </div>
+                <div class="header_navbar_links_link">
+                    <a href="show_categories.html">Categories</a>
+                </div>
+                <div class="header_navbar_links_link">
+                    <a href="show_tags.html">Tags</a>
+                </div>
+                <div class="header_navbar_links_link">
+                    <a href="show_posts.html">Posts</a>
+                </div>
+                <div class="header_navbar_links_link">
+                    <a href="show_users.html">Users</a>
+                </div>
+                <div class="header_navbar_links_link">
+                    <a href="">Statistics</a>
+                </div>
+                <div class="header_navbar_links_link">
+                    <a href="">
+                        <i class="fas fa-sign-in-alt"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main class="main">
+        @yield('content')
+    </main>
+    <footer class="footer">
+        <div class="footer_content wrapper_content">
+            <div class="footer_rights">
+                Created by Soloviev NV in 2023, all rights reserved.
+            </div>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', [App\Http\Controllers\Admin\MainController::class, 'index'])->name('index');
+});
